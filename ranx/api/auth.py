5341b419232f0bc6c637b1ef6b12c3f9437a1a10
Ameer Arsala <ameer.arsala03@gmail.com>
2024-09-01 20:57:51 -0700

    api

--- ranx/api/auth.py
+++ ranx/api/auth.py
@@ -3,7 +3,9 @@ from pydantic import BaseModel, Field
 from fastapi import APIRouter, HTTPException, status
 
 from ranx.state import AUTH_FLOW_STATE, AuthFlowState
+from ranx.constants import RAN_TOKEN_FILE_NAME, PROJECT_ROOT
 
+import json
 
 # Prefix: /auth
 router = APIRouter(tags=["Authentication"])
@@ -33,7 +35,7 @@ class AuthToken(BaseModel):
 
 class RANAuthResponse(BaseModel):
     success: bool
-    message: str
+    message: str = ""
     auth_token: Optional[AuthToken]
 
 
@@ -41,8 +43,21 @@ class RANAuthResponse(BaseModel):
 async def ran_auth_callback(auth_response: RANAuthResponse):
     global AUTH_FLOW_STATE
 
-    # TODO: handle response
+    # Handle Response
+    if auth_response.success:
+        try:
+            # Store it somewhere
+            store_token(auth_response.auth_token)
+            print("Authentication Successful!")
+        except Exception as e:
+            print(f"Error: {e}")
+    else:
+        print("Error: " + auth_response.message)
 
     # Effect: will complete any listeners
     AUTH_FLOW_STATE = AuthFlowState.SUCCESS if auth_response.success else AuthFlowState.FAILURE
 
+
+def store_token(token: AuthToken):
+    with open(f"{PROJECT_ROOT}/.ranprofile.json", 'w') as dot_ranprofile:
+        json.dump(token.dict(), dot_ranprofile)
--- ranx/constants.py
+++ ranx/constants.py
@@ -1,1 +1,9 @@
+from pathlib import Path
+
+
+PROJECT_ROOT: str = str(Path(__file__).parent.parent)
+LIB_ROOT: str = str(Path(__file__).parent)
+
 RAN_DOMAIN: str = "ran.so"
+
+RAN_TOKEN_FILE_NAME: str = ".ranprofile"
