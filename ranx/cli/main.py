49436b73f874b3ef201f20b41b651cf21761d9b3
Ameer Arsala <ameer.arsala03@gmail.com>
2024-09-01 21:25:23 -0700

    this is stupid

--- ranx/cli/main.py
+++ ranx/cli/main.py
@@ -1,25 +1,51 @@
 from typing import List, Dict, Set, Union, Optional
 
 import typer
-import uvicorn
-
 from ranx.cli.subcmds import install
+
+import uvicorn
+import asyncio
+import multiprocessing
 
 from ranx.state import AUTH_FLOW_STATE, AuthFlowState
 
+import time
 
+
+# CLI App
 app = typer.Typer(rich_markup_mode="rich")
 
 # Subcommands
 app.add_typer(install.app, name="install")
 
 
+def run_uvicorn_server(host: str, port: int):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(uvicorn.run(app, host=host, port=port))
+
+
 @app.command()
 def test():
     """For testing purposes"""
-    import subprocess
 
-    subprocess.run('uvicorn ranx.api.main:app && kill $(pgrep -P $uvicorn_pid)', shell=True)
+    def run_fastapi_server():
+        run_uvicorn_server(host="0.0.0.0", port=8000)
+    
+    fastapi_server_process = multiprocessing.Process(target=run_fastapi_server)
+    asyncio.create_task(fastapi_server_process.start())
+    
+    print("PROCESS STARTED")
+
+    async def wait():
+        await asyncio.sleep(3)
+
+    asyncio.run(wait())
+
+    print("SHUTTING DOWN")
+    asyncio.create_task(fastapi_server_process.terminate())
+    asyncio.create_task(fastapi_server_process.join())
+    print("SHUTDOWN COMPLETE.")
 
 
 @app.command()
@@ -36,6 +62,8 @@ def open_auth_server(host: str = "127.0.0.1", port: int = 8000):
         port=port,
         reload=False
     )
+
+    
 
 
 @app.command()
