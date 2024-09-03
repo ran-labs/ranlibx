904217446b237d1555625606754e596935e8a6b1
Ameer Arsala <ameer.arsala03@gmail.com>
2024-09-02 20:09:05 -0700

    made credentials be stored at a user level

--- ranx/constants.py
+++ ranx/constants.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
 
-PROJECT_ROOT: str = str(Path(__file__).parent.parent)
+RANX_PROJECT_ROOT: str = str(Path(__file__).parent.parent)
 LIB_ROOT: str = str(Path(__file__).parent)
 
 RAN_DOMAIN: str = "ran.so"
 
 RAN_TOKEN_FILE_NAME: str = ".ranprofile"
 
-RAN_TOKEN_FILEPATH_JSON: str = f"{PROJECT_ROOT}/{RAN_TOKEN_FILE_NAME}.json"
+RAN_TOKEN_FILEPATH_JSON: str = f"~/.ran/{RAN_TOKEN_FILE_NAME}.json"
