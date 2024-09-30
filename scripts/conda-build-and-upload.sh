5cd313fd78c557ac8b34dad9435b5fe0e84740a4
Ameer Arsala <ameer.arsala03@gmail.com>
2024-09-30 12:20:13 -0700

    fixed grayskull build location

--- scripts/conda-build-and-upload.sh
+++ scripts/conda-build-and-upload.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 
-# This file assumes you already have the prerequisites installed
+# NOTE: This should be executed as `scripts/conda-build-and-upload.sh`, not `./conda-build-and-upload.sh`
+# Additionally, this file assumes you already have the prerequisites installed
 
 # Find the built PyPI package
 pypi_build=$(find ./dist -maxdepth 1 -type f -name "*.tar.gz" | head -n 1)
 
 # make temp directory
-mkdir temp
-cd temp
+mkdir scripts/temp
 
 # Generate a meta.yaml from the local pypi build that already happened
-grayskull pypi "$pypi_build"
+grayskull pypi "$pypi_build" --output scripts/temp
+
+cd scripts/temp
+pwd # for debug purposes
 
 # Convert meta.yaml to a recipe.yaml
-conda-recipe-manager convert ranlibx/meta.yaml > ./recipe.yaml
+conda-recipe-manager convert ranlibx/meta.yaml > ./recipe.yaml # scripts/temp/recipe.yaml
 
 # Build it for conda with rattler-build
 rattler-build build --recipe ./recipe.yaml
 
-# move the output folder outside
-mv ./output ../scripts/output
+# move the output folder outside (scripts/temp/output -> scripts/output)
+mv ./output ../output
 
-# go back and remove the directory
-cd ..
+# go back to root and remove temp directory
+cd ../..
 rm -rf temp
 
 # Get the .conda file to upload
