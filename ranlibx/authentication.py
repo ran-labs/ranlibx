5ee06cdf80ec13ff46368399c3759bb5f83810bb
Ameer Arsala <ameer.arsala03@gmail.com>
2024-10-12 20:30:16 -0700

    finished token validation

--- ranlibx/authentication.py
+++ ranlibx/authentication.py
@@ -3,7 +3,7 @@ import json
 import httpx
 
 from ranlibx.api.schemas.token import AuthToken
-from ranlibx.constants import RAN_AUTH_TOKEN_FILEPATH_JSON
+from ranlibx.constants import RAN_AUTH_TOKEN_FILEPATH_JSON, RAN_API_SERVER_URL
 
 
 def store_token(token: AuthToken):
@@ -11,9 +11,38 @@ def store_token(token: AuthToken):
         json.dump(token.dict(), dot_ranprofile)
 
 
+def read_token() -> AuthToken:
+    with open(RAN_AUTH_TOKEN_FILEPATH_JSON, 'r') as dot_ranprofile:
+        data: dict = json.load(dot_ranprofile)
+    
+    auth_token: AuthToken = AuthToken(**data)
+    
+    # some small checking just in case
+    MIN_TOKEN_LEN: int = 5
+    if len(auth_token.token) < MIN_TOKEN_LEN:
+        raise Exception("Not a token")
+    
+    return auth_token
+
+
 def is_token_valid(token: AuthToken) -> bool:
-    # TODO: this
-    pass
+    # Check if valid
+    try:
+        res = httpx.post(
+            url=f"{RAN_API_SERVER_URL}/v1/auth/cli/are_credentials_valid",
+            headers={"Authorization": f"Bearer {token.token}"}
+        )
+
+        if not res.is_success:
+            return False
+
+        res_data: dict = res.json()
+        if not res_data.get("valid"):
+            return False
+
+        return True
+    except:
+        return False
 
 
 def authenticate(token: AuthToken, verbose: bool = True) -> bool:
--- ranlibx/constants.py
+++ ranlibx/constants.py
@@ -4,7 +4,7 @@ RANLIBX_PROJECT_ROOT: str = str(Path(__file__).parent.parent)
 LIB_ROOT: str = str(Path(__file__).parent)
 
 RAN_DOMAIN: str = "ran.so"
+RAN_API_SERVER_URL: str = "https://lib.ran.so"
 
-RAN_TOKEN_FILE_NAME: str = ".ranprofile"
-
-RAN_AUTH_TOKEN_FILEPATH_JSON: str = f"~/.ran/{RAN_TOKEN_FILE_NAME}.json"
+RAN_AUTH_TOKEN_FILE_NAME: str = ".ranprofile"
+RAN_AUTH_TOKEN_FILEPATH_JSON: str = f"~/.ran/{RAN_AUTH_TOKEN_FILE_NAME}.json"
